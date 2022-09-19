a9d09dfcaee3c19added467a5b644b9e965db44b
otaviohrg <otavio@truepay.com.br>
2022-09-19 12:13:46 +0200

    feat: add module to handle celestial bodies entries in Pickle database

--- astronomical_simulation_backend/infrastructure/database/celestial_body_repository.py
+++ astronomical_simulation_backend/infrastructure/database/celestial_body_repository.py
@@ -1,28 +1,42 @@
 import pickle
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional, List
 
-from astronomical
-from myapp.domain.todo_entry_repository import ITodoEntryRepository
+from astronomical_simulation_backend.domain.celestial_body import CelestialBody
+from astronomical_simulation_backend.domain.celestial_body_repository import ICelestialBodyRepository
 
 
-class TodoEntryNotFound(Exception):
+class CelestialBodyNotFound(Exception):
     pass
 
 
 @dataclass
-class TodoEntryPickleRepository(ITodoEntryRepository):
+class TodoEntryPickleRepository(ICelestialBodyRepository):
     storage_dir: str
 
-    def get(self, entry_id: str) -> TodoEntry:
+    def get(self, entry_id: str) -> CelestialBody:
         try:
-            entry: TodoEntry
+            entry: CelestialBody
             with open(Path(self.storage_dir) / entry_id) as entry_file:
                 entry = pickle.load(entry_file)
             return entry
         except Exception:
-            raise TodoEntryNotFound()
+            raise CelestialBodyNotFound()
 
-    def add(self, entry: TodoEntry) -> None:
+    def add(self, entry: CelestialBody) -> None:
         with open(Path(self.storage_dir) / entry.id) as entry_file:
-            pickle.dump(entry, entry_file
+            pickle.dump(entry, entry_file)
+
+    def get_all(self, search: Optional[str]) -> List[CelestialBody]:
+        entries: List[CelestialBody] = []
+        for entry_file_path in Path(self.storage_dir).iterdir():
+            with open(entry_file_path, mode="rb") as entry_file:
+                entry: CelestialBody = pickle.load(entry_file)
+                if search:
+                    if search in entry.name:
+                        entries.append(entry)
+                else:
+                    entries.append(entry)
+        return entries
+    
