f4307d7efe00734cba0ca5e86f068ed593be54c2
otaviohrg <otavio@truepay.com.br>
2022-09-27 14:56:27 +0200

    feat: add simulation controller to api

--- astronomical_simulation_backend/infrastructure/api/setup.py
+++ astronomical_simulation_backend/infrastructure/api/setup.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 
 from astronomical_simulation_backend.container import ApplicationContainer
-from astronomical_simulation_backend.infrastructure.api import celestial_body_controller
+from astronomical_simulation_backend.infrastructure.api import (
+    celestial_body_controller,
+    simulation_controller
+)
 
 
 def setup(app: FastAPI, container: ApplicationContainer) -> None:
     # Add other controllers here
     app.include_router(celestial_body_controller.router)
+    app.include_router(simulation_controller.router)
 
     # Inject dependencies
     container.wire(
         modules=[
             celestial_body_controller,
+            simulation_controller
         ]
     )
