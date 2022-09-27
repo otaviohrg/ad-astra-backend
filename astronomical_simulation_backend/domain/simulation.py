05467a5b71ca6e372b66e94bff27270544a72c7e
otaviohrg <otavio@truepay.com.br>
2022-09-27 09:11:37 +0200

    chore: remove dependency from domain layer

--- astronomical_simulation_backend/domain/simulation.py
+++ astronomical_simulation_backend/domain/simulation.py
@@ -1,10 +1,7 @@
 import uuid
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Dict
-from astronomical_simulation_backend.domain.constants import SAMPLE_TIME
-
-from astronomical_simulation_backend.domain.celestial_body import CelestialBody
+from typing import Dict
 
 
 @dataclass
@@ -24,17 +21,3 @@ class Simulation:
             degree=float(params["degree"]),
             status="STOPPED"
         )
-
-    def update(self, celestial_bodies: List[CelestialBody]) -> None:
-        for body in celestial_bodies:
-            body.set_velocity(celestial_bodies, self.degree, self.k)
-            body.move()
-
-    def run(self, celestial_bodies: List[CelestialBody], duration: float) -> List[CelestialBody]:
-        t = 0.0
-        while t < duration:
-            self.update(celestial_bodies)
-            for body in celestial_bodies:
-                body.add_position_to_trajectory()
-            t += SAMPLE_TIME
-        return celestial_bodies
