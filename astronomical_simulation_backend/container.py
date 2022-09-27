7e9a1f5c68b37895f26c73de36f6d23893f64f91
otaviohrg <otavio@truepay.com.br>
2022-09-27 11:05:05 +0200

    feat: add simulation service to application container

--- astronomical_simulation_backend/container.py
+++ astronomical_simulation_backend/container.py
@@ -3,8 +3,14 @@ from dependency_injector import containers, providers
 from astronomical_simulation_backend.application.celestial_body_service import (
     CelestialBodyService,
 )
+from astronomical_simulation_backend.application.simulation_service import (
+    SimulationService,
+)
 from astronomical_simulation_backend.infrastructure.database.celestial_body_repository import (
     CelestialBodyPickleRepository,
+)
+from astronomical_simulation_backend.infrastructure.database.simulation_repository import (
+    SimulationPickleRepository,
 )
 
 
@@ -12,9 +18,22 @@ class ApplicationContainer(containers.DeclarativeContainer):
     configuration = providers.Configuration()
 
     celestial_body_repository = providers.Singleton(
-        CelestialBodyPickleRepository, storage_dir=configuration.storage_dir
+        CelestialBodyPickleRepository,
+        storage_dir=configuration.storage_dir + "/celestial_bodies"
+    )
+
+    simulation_repository = providers.Singleton(
+        SimulationPickleRepository,
+        storage_dir=configuration.storage_dir + "/simulation"
     )
 
     celestial_body_service = providers.Factory(
-        CelestialBodyService, celestial_body_repository
+        CelestialBodyService,
+        celestial_body_repository=celestial_body_repository
+    )
+
+    simulation_repository = providers.Factory(
+        SimulationService,
+        celestial_body_repository=celestial_body_repository,
+        simulation_repository=simulation_repository
     )
